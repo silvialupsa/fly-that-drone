dac740ae8ef2f671ead47dc4835331663413adf3
Silvia Lupsa <lupsasilvia@gmail.com>
2023-06-29 10:27:43 +0300

    moving forward

--- src/main/java/com/codecool/ftd/logic/MovementEngine.java
+++ src/main/java/com/codecool/ftd/logic/MovementEngine.java
@@ -1,15 +1,20 @@
 package com.codecool.ftd.logic;
 
 import com.codecool.ftd.data.Drone;
+import com.codecool.ftd.data.Position;
 
 import java.util.List;
 
 public class MovementEngine {
 
     public void move(Drone drone, String command) {
+        switch (command){
+            case "FORWARD": drone.setPosition(new Position(drone.getPosition().x()-1, drone.getPosition().y(), drone.getPosition().z()));
+        }
+
     }
 
     public List<String> getCommands() {
-        return List.of("No available commands yet.");
+        return List.of("FORWARD", "BACKWARD", "LEFT", "RIGHT" );
     }
 }
