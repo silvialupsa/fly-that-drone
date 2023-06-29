6de459c60dd33f401f5120e71549511932390a5a
Silvia Lupsa <lupsasilvia@gmail.com>
2023-06-29 10:32:14 +0300

    moving left

--- src/main/java/com/codecool/ftd/logic/MovementEngine.java
+++ src/main/java/com/codecool/ftd/logic/MovementEngine.java
@@ -11,7 +11,7 @@ public class MovementEngine {
         switch (command){
             case "FORWARD": drone.setPosition(new Position(drone.getPosition().x()-1, drone.getPosition().y(), drone.getPosition().z()));
             case "BACKWARD": drone.setPosition(new Position(drone.getPosition().x()+1, drone.getPosition().y(), drone.getPosition().z()));
-            
+            case "LEFT":drone.setPosition(new Position(drone.getPosition().x(), drone.getPosition().y()+1, drone.getPosition().z()));
         }
 
     }
