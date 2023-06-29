b750ac8a4452e9dd4eef4a859ded7f8bd417844d
Silvia Lupsa <lupsasilvia@gmail.com>
2023-06-29 11:43:03 +0300

    basics movements

--- src/main/java/com/codecool/ftd/logic/MovementEngine.java
+++ src/main/java/com/codecool/ftd/logic/MovementEngine.java
@@ -9,14 +9,13 @@ public class MovementEngine {
 
     public void move(Drone drone, String command) {
         switch (command){
-            case "FORWARD": drone.setPosition(new Position(drone.getPosition().x()-1, drone.getPosition().y(), drone.getPosition().z()));
-            case "BACKWARD": drone.setPosition(new Position(drone.getPosition().x()+1, drone.getPosition().y(), drone.getPosition().z()));
-            case "LEFT":drone.setPosition(new Position(drone.getPosition().x(), drone.getPosition().y()+1, drone.getPosition().z()));
-            case "RIGHT":drone.setPosition(new Position(drone.getPosition().x(), drone.getPosition().y()+1, drone.getPosition().z()));
+            case "FORWARD"->drone.setPosition(new Position(drone.getPosition().x()-1, drone.getPosition().y(), drone.getPosition().z()));
+            case "BACKWARD"->drone.setPosition(new Position(drone.getPosition().x()+1, drone.getPosition().y(), drone.getPosition().z()));
+            case "LEFT"->drone.setPosition(new Position(drone.getPosition().x(), drone.getPosition().y()+1, drone.getPosition().z()));
+            case "RIGHT"->drone.setPosition(new Position(drone.getPosition().x(), drone.getPosition().y()-1, drone.getPosition().z()));
+
         }
-
     }
-
     public List<String> getCommands() {
         return List.of("FORWARD", "BACKWARD", "LEFT", "RIGHT" );
     }
